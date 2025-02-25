320e3d5a6cca51b7330cd4da17c0a167964ad2ea
ФAnatoli Afonenko <afonenkoaa@bsu.by>
2025-02-25 19:07:17 +0300

    Проведён рефакторинг проекта. Добавлены шаблоны для проверочных работ и включения решений. Начата пререработка сслыок на библиотечные функции через пакет downlit.

--- scripts/widgets/resolve-lib-ref.R
+++ scripts/widgets/resolve-lib-ref.R
@@ -1,11 +1,14 @@
 library(fs)
 library(purrr)
 library(readr)
+library(stringr)
 
 out <- Sys.getenv('QUARTO_PROJECT_OUTPUT_DIR')
-lib_path <- switch(Sys.getenv('QUARTO_PROFILE'),
-                   'production' = Sys.getenv('PROJECT_PATH'),
-                   'development' = path_abs(out)) |>
+prof <- Sys.getenv('QUARTO_PROFILE') |> str_split_1(',')
+prof_path <- list('dev' = path_abs(out),
+     'prod' = Sys.getenv('PROJECT_PATH'))
+
+lib_path <- prof_path[[intersect(names(prof_path), prof)]] |>
   path(Sys.getenv('SITE_LIBS_PATH'))
 
 substitue_lib_path <- function(p)
