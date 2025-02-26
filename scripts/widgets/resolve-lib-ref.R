da82b1aa11c80f1d13fa6bdac3c717dd9ad7bb56
ФAnatoli Afonenko <afonenkoaa@bsu.by>
2025-02-26 13:36:13 +0300

    Добавлена секция RStudio.

--- scripts/widgets/resolve-lib-ref.R
+++ scripts/widgets/resolve-lib-ref.R
@@ -3,6 +3,19 @@ library(purrr)
 library(readr)
 library(stringr)
 
+
+if (!nzchar(Sys.getenv("QUARTO_PROJECT_RENDER_ALL")))
+  quit()
+
+has_html_out <- Sys.getenv('QUARTO_PROJECT_OUTPUT_FILES') |>
+  stringr::str_split_1('\\\\') |>
+  path_ext() |>
+  unique() |>
+  is.element('html', set=_)
+
+if(!has_html_out)
+  quit()
+
 out <- Sys.getenv('QUARTO_PROJECT_OUTPUT_DIR')
 prof <- Sys.getenv('QUARTO_PROFILE') |> str_split_1(',')
 prof_path <- list('dev' = path_abs(out),
